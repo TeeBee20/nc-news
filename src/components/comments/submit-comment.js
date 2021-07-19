3c6fab8a48dcfb6e7777fa4daf8b3fac9fb6e5c0
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-07-19 10:04:08 +0100

    add error handling for posting a blank comment

--- src/components/comments/submit-comment.js
+++ src/components/comments/submit-comment.js
@@ -1,17 +1,16 @@
-import { useContext} from "react";
+import { useContext } from "react";
 import { ExpandContext } from "../misc/Expand";
 import { UserContext } from "../misc/user";
 import useComment from "../../hooks/useComment";
 import { postCommentByArticleId } from "../utils";
 import { useParams } from "react-router";
-import useError from '../../hooks/useError';
+import useError from "../../hooks/useError";
 
 const SubmitComment = () => {
   const { user } = useContext(UserContext);
   const { commentsClicked, setComments } = useContext(ExpandContext);
-  const { commentBody, setCommentBody } =
-    useComment();
-    const {hasError, setHasError} = useError();
+  const { commentBody, setCommentBody } = useComment();
+  const { hasError, setHasError } = useError();
   const { article_id } = useParams();
 
   const handleSubmit = (event) => {
@@ -20,21 +19,26 @@ const SubmitComment = () => {
       username: user,
       body: commentBody,
     };
-    postCommentByArticleId(article_id, comment)
-      .then((comment) => {
-        setHasError(false);
-        setComments((currComments) => {
-          return [comment[0], ...currComments];
+
+    if (commentBody.length === 0) {
+      setHasError(true);
+    } else {
+      postCommentByArticleId(article_id, comment)
+        .then((comment) => {
+          setHasError(false);
+          setComments((currComments) => {
+            return [comment[0], ...currComments];
+          });
+        })
+        .catch((err) => {
+          setHasError(true);
         });
-      })
-      .catch((err) => {
-        setHasError(true);
-      });
+    }
   };
 
   return (
     <div>
-    {hasError && <p>Oops! Couldn't submit comment.</p>}
+      {hasError && <p>Oops! Couldn't submit comment.</p>}
       {commentsClicked && (
         <form onSubmit={handleSubmit}>
           <input
--- src/components/home/articles.js
+++ src/components/home/articles.js
@@ -2,73 +2,90 @@ import {
   fetchAllArticles,
   fetchArticlesByTopic,
   sortArticlesByQuery,
-  articleImage
+  articleImage,
 } from "../utils";
-import useLoading from '../../hooks/useLoading';
+import useLoading from "../../hooks/useLoading";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import useError from '../../hooks/useError';
+import useError from "../../hooks/useError";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
-  const { loading, setLoading} = useLoading();
-  const {hasError, setHasError} = useError();
+  const { loading, setLoading } = useLoading();
+  const { hasError, setHasError } = useError();
   const { topic_slug } = useParams();
 
   useEffect(() => {
     if (topic_slug) {
-      fetchArticlesByTopic(topic_slug).then((articles) => {
-        setHasError(false);
-        setLoading(false);
-        setArticles(articles);
-        }).catch(err => {
-          if(err) {
+      fetchArticlesByTopic(topic_slug)
+        .then((articles) => {
+          setHasError(false);
+          setLoading(false);
+          setArticles(articles);
+        })
+        .catch((err) => {
+          if (err) {
             setHasError(true);
           }
         });
     } else {
-      fetchAllArticles().then((articles) => {
-        setHasError(false);
-         setLoading(false);
-        setArticles(articles);
-        }).catch(err => {
-          if(err) {
+      fetchAllArticles()
+        .then((articles) => {
+          setHasError(false);
+          setLoading(false);
+          setArticles(articles);
+        })
+        .catch((err) => {
+          if (err) {
             setHasError(true);
           }
         });
     }
   }, [topic_slug]);
-  
+
   const sort = (event) => {
     sortArticlesByQuery(event.target.value).then((articles) => {
       setArticles(articles);
     });
   };
 
- 
+  console.log(articleImage("coding"));
 
   return (
     <div className="Articles">
       <h3>Sort By:</h3>
-      <button value="created_at" onClick={sort}>Date</button>
-      <button value="comment_count" onClick={sort}>Comments</button>
-      <button value="votes" onClick={sort}>Votes</button>
-      {!loading ? <ul>
-        {articles.map((article) => {
-          return (
-            <Link to={`/${article.topic}/${article.article_id}`} key={article.article_id}>
-              <li>
-                <p>{article.topic}</p>
-                <p>{article.author}</p>
-                <img src={() => {articleImage(topic_slug)}} alt="image" />
-                <h3>{article.title}</h3>
-                <p>Votes: {article.votes}</p>
-                <p>Comments: {article.comment_count}</p>
-              </li>
-            </Link>
-          );
-        })}
-      </ul> : <p>Loading...</p>}
+      <button value="created_at" onClick={sort}>
+        Date
+      </button>
+      <button value="comment_count" onClick={sort}>
+        Comments
+      </button>
+      <button value="votes" onClick={sort}>
+        Votes
+      </button>
+      {!loading ? (
+        <ul>
+          {articles.map((article) => {
+            return (
+              <Link
+                to={`/${article.topic}/${article.article_id}`}
+                key={article.article_id}
+              >
+                <li>
+                  <p>{article.topic}</p>
+                  <p>{article.author}</p>
+                  {/* <img src={articleImage(topic_slug)} alt="image" /> */}
+                  <h3>{article.title}</h3>
+                  <p>Votes: {article.votes}</p>
+                  <p>Comments: {article.comment_count}</p>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
       {hasError && <p>Oops! Couldn't load articles.</p>}
     </div>
   );
